1880aaaf245ef7754ddd3dae7c8f41c90dfffb83
interntech <darrenho97@hotmail.com>
2019-04-27 15:16:29 +0800

    first page css and html done

--- app.py
+++ app.py
@@ -8,18 +8,22 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
+
 def home():
 	# default directory for home page
 	return render_template("Home.html", title = "Welcome Back")
+
+@app.route('/stocks')
 
 def stocks():
 	# directing user to page 2 (list of stocks)
 	return render_template("Stocks.html", title = "Stocks Listing")
 
+@app.route('/portfolio')
+
 def holdings():
 	# directing user to page 3 (portfolio / list of stocks purchased / history)
 	return render_template("Portfolio.html", title = "Stocks Portfolio / Holdings")
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
