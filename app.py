3109b4d3bad1d4cb11a039aade658acf4f9742b2
Jun Qing Lim <itsjunqing@gmail.com>
2019-04-27 12:21:36 +0800

    Added gitignore and some home page codes

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+
+class User:
+	balance = 10000
+	stocks_value = 5000
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+	# default directory for home page
+	return render_template("Home.html", title = "Welcome Back")
+
+def stocks():
+	# directing user to page 2 (list of stocks)
+	return render_template("Stocks.html", title = "Stocks Listing")
+
+def holdings():
+	# directing user to page 3 (portfolio / list of stocks purchased / history)
+	return render_template("Portfolio.html", title = "Stocks Portfolio / Holdings")
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run(debug=True)
